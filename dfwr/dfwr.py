1a64e4001e9b6977424327e17b49390db7570c89
velhorst <rufusvelhorst@gmail.com>
2017-01-10 14:02:38 +0100

    Updated and extended
    Updated: __init__, get_var, set_var, update,
    added: com_file_update, get_time_step

--- dfwr/dfwr.py
+++ dfwr/dfwr.py
@@ -1,6 +1,8 @@
 from bmi.wrapper import BMIWrapper
 from bmi.api import IBmi
-
+import xarray
+import os
+from glob import glob
 
 class DFWR(IBmi):
 
@@ -8,12 +10,59 @@ class DFWR(IBmi):
         
         self.configfile = configfile
 
-        # load dflow
-        self.dflow = BMIWrapper(self.configfile)
+        # TODO: make configfile 
+            # Needed from configfile:
+            # ARCH, D3D_HOME, dflowfmexedir, 
+            # flow_dir, mdu, 
+            # wave_dir, mdw
+            
+        ARCH=  r'win64'
+        D3D_HOME= r'C:\vanArjen-Dflow_setup\fm_versions\3Nov'
+        dflowfmexedir= r'c:\fm_versions\dflowfm-lib-x64-1.1.192.48947\dflowfm-lib-x64-1.1.192.48947'
+        flow_dir = r'c:\Users\velhorst\Desktop\PythonScripts\BMI\models\SandEngine\DF'
+        wave_dir = r'c:\Users\velhorst\Desktop\PythonScripts\BMI\models\SandEngine\WV'
+        mdu = r'c:\Users\velhorst\Desktop\PythonScripts\BMI\models\SandEngine\DF\zm_dfm_used.mdu'
+        mdw = r'c:\Users\velhorst\Desktop\PythonScripts\BMI\models\SandEngine\WV\zm_dfm_used.mdw'
+        self.flow_dir = flow_dir
+        self.wave_dir = wave_dir
+        
+        #==============================================================================
+        # Set paths and environmental parameters
+        #==============================================================================
+        waveexedir=os.path.join(D3D_HOME,ARCH,r'wave\bin')
+        swanexedir=os.path.join(D3D_HOME,ARCH,r'swan\bin')
+        swanbatdir=os.path.join(D3D_HOME,ARCH,r'swan\scripts')
+        esmfexedir=os.path.join(D3D_HOME,ARCH,r'esmf\bin')
+        esmfbatdir=os.path.join(D3D_HOME,ARCH,r'esmf\scripts')
+        
+        pathlist = []
+        for name in [dflowfmexedir,waveexedir,swanexedir,swanbatdir,esmfexedir,esmfbatdir]:
+            pathlist.append(name)
+        
+        # append the paths to the bmiwrapper
+        for path in pathlist:
+            BMIWrapper.known_paths.append(path)
+        
+#        # store original environmental parameters, to be able to replace them back at the end of the script
+#        original_ARCH = os.environ['ARCH']
+#        original_D3D_HOME = os.environ['D3D_HOME']
+#        original_PATH = os.environ['PATH']
+        
+        # Set environmental parameters    
+        os.environ['ARCH'] = ARCH
+        os.environ['D3D_HOME'] = D3D_HOME
+        os.environ['swanexedir'] = swanexedir
+        os.environ['swanbatdir'] = swanbatdir
+        os.environ['PATH'] += os.pathsep + os.pathsep.join(pathlist)
 
-        # load dwaves
-        self.dwaves = BMIWrapper(self.configfile) # TODO: we might need to use a different configfile here, but be aware that the __init__ method can only take one parameter!
+        #load dflow
+        os.chdir(self.flow_dir)
+        self.dflow = BMIWrapper(engine="dflowfm", configfile=mdu)
 
+        #load dwaves
+        os.chdir(self.wave_dir)
+        self.dwaves = BMIWrapper(engine="wave", configfile=mdw)
+        
         
     def __enter__(self):
         self.initialize()
@@ -29,6 +78,8 @@ class DFWR(IBmi):
     def get_current_time(self):
         return self.dflow.get_current_time()
     
+    def get_time_step(self):
+        return self.dflow.get_time_step()
     
     def get_start_time(self):
         return self.dflow.get_start_time()
@@ -40,10 +91,7 @@ class DFWR(IBmi):
     
     def get_var(self, var):
         
-        if var in []: # TODO: fill this list with appropriate variable names
-            pass # TODO: do something with communication file
-        else:
-            return self.dflow.get_var(var)
+        return self.dflow.get_var(var)
     
     
     def get_var_name(self, i):
@@ -78,11 +126,21 @@ class DFWR(IBmi):
 
     
     def set_var(self, var, val):
-
-        if var in []: # TODO: fill this list with appropriate variable names
-            pass # TODO: do something with communication file
-        else:
-            self.dflow.set_var(var, val)
+        # TODO: 
+        # there is now a difference between the dflow variable and the dwaves 
+        # Dflow is correctly updated for the next timestep, but the updating of
+        # the dwaves variable is only after dflow writes to the com-file. 
+        # Only the bed level is updated immediately in the com-file and is 
+        # correctly set in dwaves.
+        # To fix this the updating of the com-file should be done manually for 
+        # all variables used by dwaves.
+        
+        if var in ['bl']: # DONE: fill this list with appropriate variable names
+#            # DONE: do something with communication file
+            com_var = ['FlowElem_zcc']
+            com_file_update(com_var,-val)# minus because of different definition
+        
+        self.dflow.set_var(var, val)
     
         
     def set_var_index(self, var, idx):
@@ -102,17 +160,55 @@ class DFWR(IBmi):
 
         # initialize dwaves
         self.dwaves.initialize()
-
-            
+        
+        # find _com.nc file
+        output_DF = os.path.join(self.flow_dir,r'DFM_OUTPUT_{}'.format(os.path.split(mdu)[1][:-4]))
+        com_nc_locater = os.path.join(output_DF.replace(' ',''),r'*_com.nc')# quick fix with replace (something because of an extra space)
+        com_nc = glob(com_nc_locater)
+        self.com_file = com_nc[0]
+        
     def update(self, dt=-1):
-
+        # for starters: use the exchange interval between dflow and 
+        # dwaves as dt. E.g. 1200 seconds
+        # com_file is the location of the communication file
+        
+        # update dwaves
+        os.chdir(self.wave_dir)   
+        if self.dflow.get_current_time() ==0.:
+            self.dwaves.update(0)
+            self.dflow.update(self.dflow.get_current_time()) # might be unnecessary
+        else:
+            self.dwaves.update(dt) # TODO: maybe we need some timestep differentiation here?
+        # ANSWER: not necessarily. But to be sure the dflow user timestep could be used for dflow
+                       
         # update dflow
+        os.chdir(self.flow_dir)   
         self.dflow.update(dt)
 
-        # update dwaves
-        self.dwaves.update(dt) # TODO: maybe we need some timestep differentiation here?
-
+        # get dflow bed level to set in com-file
+        bl_dflow = self.dflow.get_var('bl')
         
+        # update bed level in com-file
+        com_file_update('FlowElem_zcc',-bl_dflow) # minus because of different definition
+        
+        
+        
+    def com_file_update(self, var, val):
+#        update the bed in the com-file with 'val'
+        
+        fname = self.com_file
+        with xarray.open_dataset(fname) as ds:
+            data = ds.copy(deep=True)
+        # Problem encountered: variable 'windyu' has a attribute called 'coordinates'
+        # This is problematic because the "coordinates" (eg x and y)  cannot be serialized anymore
+        # ugly solution: remove this attribute only
+        # Fairly better solution: remove all attributes called 'coordinates'
+        # Best solution (but I don't know how): do not create this attribute in the first place
+            for var_name in data.variables.keys():
+                data[var_name].attrs.pop('coordinates',None)    
+            data[var].values = val 
+            data.to_netcdf(fname,format="NETCDF3_CLASSIC")
+    
     def finalize(self):
 
         # finalize dflow
