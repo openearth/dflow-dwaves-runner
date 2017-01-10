d169e706e1b9970eca0c736c82884aa1f331fe8b
Bas Hoonhout <bas@hoonhout.com>
2017-01-10 14:30:22 +0100

    Added reading of JSON config file

--- dfwr/dfwr.py
+++ dfwr/dfwr.py
@@ -1,8 +1,10 @@
+import os
+import json
+import xarray
+from glob import glob
 from bmi.wrapper import BMIWrapper
 from bmi.api import IBmi
-import xarray
-import os
-from glob import glob
+
 
 class DFWR(IBmi):
 
@@ -10,58 +12,48 @@ class DFWR(IBmi):
         
         self.configfile = configfile
 
-        # TODO: make configfile 
-            # Needed from configfile:
-            # ARCH, D3D_HOME, dflowfmexedir, 
-            # flow_dir, mdu, 
-            # wave_dir, mdw
-            
-        ARCH=  r'win64'
-        D3D_HOME= r'C:\vanArjen-Dflow_setup\fm_versions\3Nov'
-        dflowfmexedir= r'c:\fm_versions\dflowfm-lib-x64-1.1.192.48947\dflowfm-lib-x64-1.1.192.48947'
-        flow_dir = r'c:\Users\velhorst\Desktop\PythonScripts\BMI\models\SandEngine\DF'
-        wave_dir = r'c:\Users\velhorst\Desktop\PythonScripts\BMI\models\SandEngine\WV'
-        mdu = r'c:\Users\velhorst\Desktop\PythonScripts\BMI\models\SandEngine\DF\zm_dfm_used.mdu'
-        mdw = r'c:\Users\velhorst\Desktop\PythonScripts\BMI\models\SandEngine\WV\zm_dfm_used.mdw'
-        self.flow_dir = flow_dir
-        self.wave_dir = wave_dir
-        
-        #==============================================================================
-        # Set paths and environmental parameters
-        #==============================================================================
-        waveexedir=os.path.join(D3D_HOME,ARCH,r'wave\bin')
-        swanexedir=os.path.join(D3D_HOME,ARCH,r'swan\bin')
-        swanbatdir=os.path.join(D3D_HOME,ARCH,r'swan\scripts')
-        esmfexedir=os.path.join(D3D_HOME,ARCH,r'esmf\bin')
-        esmfbatdir=os.path.join(D3D_HOME,ARCH,r'esmf\scripts')
-        
+        # read json file
+        if os.path.exists(self.configfile):
+            with open(self.configfile, 'r') as fp:
+                self.cfg = json.load(fp)
+        else:
+            raise IOError('File not found: %s', self.configfile)
+
+        # set run directories
+        self.flow_dir = self.cfg['flow_dir']
+        self.wave_dir = self.cfg['wave_dir']
+
+        # set exe directories
+        waveexedir = os.path.join(self.cfg['D3D_HOME'], self.cfg['ARCH'], r'wave\bin')
+        swanexedir = os.path.join(self.cfg['D3D_HOME'], self.cfg['ARCH'], r'swan\bin')
+        swanbatdir = os.path.join(self.cfg['D3D_HOME'], self.cfg['ARCH'], r'swan\scripts')
+        esmfexedir = os.path.join(self.cfg['D3D_HOME'], self.cfg['ARCH'], r'esmf\bin')
+        esmfbatdir = os.path.join(self.cfg['D3D_HOME'], self.cfg['ARCH'], r'esmf\scripts')
+
+        # store path list
         pathlist = []
         for name in [dflowfmexedir,waveexedir,swanexedir,swanbatdir,esmfexedir,esmfbatdir]:
             pathlist.append(name)
         
-        # append the paths to the bmiwrapper
         for path in pathlist:
             BMIWrapper.known_paths.append(path)
-        
-#        # store original environmental parameters, to be able to replace them back at the end of the script
-#        original_ARCH = os.environ['ARCH']
-#        original_D3D_HOME = os.environ['D3D_HOME']
-#        original_PATH = os.environ['PATH']
-        
-        # Set environmental parameters    
-        os.environ['ARCH'] = ARCH
-        os.environ['D3D_HOME'] = D3D_HOME
+
+        # set environment variables
+        os.environ['ARCH'] = self.cfg['ARCH']
+        os.environ['D3D_HOME'] = self.cfg['D3D_HOME']
         os.environ['swanexedir'] = swanexedir
         os.environ['swanbatdir'] = swanbatdir
         os.environ['PATH'] += os.pathsep + os.pathsep.join(pathlist)
 
-        #load dflow
+        # load dflow
         os.chdir(self.flow_dir)
-        self.dflow = BMIWrapper(engine="dflowfm", configfile=mdu)
+        self.dflow = BMIWrapper(engine="dflowfm", configfile=os.path.join(self.cfg['flow_dir'],
+                                                                          self.cfg['mdu_file']))
 
-        #load dwaves
+        # load dwaves
         os.chdir(self.wave_dir)
-        self.dwaves = BMIWrapper(engine="wave", configfile=mdw)
+        self.dwaves = BMIWrapper(engine="wave", configfile=os.path.join(self.cfg['wave_dir'],
+                                                                        self.cfg['mdw_file']))
         
         
     def __enter__(self):
@@ -77,9 +69,11 @@ class DFWR(IBmi):
         
     def get_current_time(self):
         return self.dflow.get_current_time()
+
     
     def get_time_step(self):
         return self.dflow.get_time_step()
+
     
     def get_start_time(self):
         return self.dflow.get_start_time()
@@ -90,7 +84,6 @@ class DFWR(IBmi):
     
     
     def get_var(self, var):
-        
         return self.dflow.get_var(var)
     
     
@@ -135,10 +128,9 @@ class DFWR(IBmi):
         # To fix this the updating of the com-file should be done manually for 
         # all variables used by dwaves.
         
-        if var in ['bl']: # DONE: fill this list with appropriate variable names
-#            # DONE: do something with communication file
-            com_var = ['FlowElem_zcc']
-            com_file_update(com_var,-val)# minus because of different definition
+        if var in self.cfg['com_structure'].keys():
+            com_var = self.cfg['com_structure'][var]
+            self._update_com_file(com_var, val)
         
         self.dflow.set_var(var, val)
     
@@ -162,53 +154,35 @@ class DFWR(IBmi):
         self.dwaves.initialize()
         
         # find _com.nc file
-        output_DF = os.path.join(self.flow_dir,r'DFM_OUTPUT_{}'.format(os.path.split(mdu)[1][:-4]))
-        com_nc_locater = os.path.join(output_DF.replace(' ',''),r'*_com.nc')# quick fix with replace (something because of an extra space)
+        output_DF = os.path.join(self.flow_dir, r'DFM_OUTPUT_{}'.format(os.path.splitext(self.cfg['mdu_file'])[0]))
+        com_nc_locater = os.path.join(output_DF, r'*_com.nc')
         com_nc = glob(com_nc_locater)
         self.com_file = com_nc[0]
+
         
-    def update(self, dt=-1):
+    def update(self, dt=1200):
         # for starters: use the exchange interval between dflow and 
         # dwaves as dt. E.g. 1200 seconds
         # com_file is the location of the communication file
         
         # update dwaves
         os.chdir(self.wave_dir)   
-        if self.dflow.get_current_time() ==0.:
+        if self.dflow.get_current_time() == 0.:
             self.dwaves.update(0)
             self.dflow.update(self.dflow.get_current_time()) # might be unnecessary
         else:
-            self.dwaves.update(dt) # TODO: maybe we need some timestep differentiation here?
-        # ANSWER: not necessarily. But to be sure the dflow user timestep could be used for dflow
+            self.dwaves.update(dt)
                        
         # update dflow
-        os.chdir(self.flow_dir)   
+        os.chdir(self.flow_dir)
         self.dflow.update(dt)
 
-        # get dflow bed level to set in com-file
-        bl_dflow = self.dflow.get_var('bl')
-        
-        # update bed level in com-file
-        com_file_update('FlowElem_zcc',-bl_dflow) # minus because of different definition
-        
+        # get dflow bed level to set in com-file, update bed level in com-file
+        for vf, vc in self.cfg['com_structure'].iteritems():
+            val = self.dflow.get_var(vf)
+            self._update_com_file(vc, val)
         
         
-    def com_file_update(self, var, val):
-#        update the bed in the com-file with 'val'
-        
-        fname = self.com_file
-        with xarray.open_dataset(fname) as ds:
-            data = ds.copy(deep=True)
-        # Problem encountered: variable 'windyu' has a attribute called 'coordinates'
-        # This is problematic because the "coordinates" (eg x and y)  cannot be serialized anymore
-        # ugly solution: remove this attribute only
-        # Fairly better solution: remove all attributes called 'coordinates'
-        # Best solution (but I don't know how): do not create this attribute in the first place
-            for var_name in data.variables.keys():
-                data[var_name].attrs.pop('coordinates',None)    
-            data[var].values = val 
-            data.to_netcdf(fname,format="NETCDF3_CLASSIC")
-    
     def finalize(self):
 
         # finalize dflow
@@ -217,3 +191,26 @@ class DFWR(IBmi):
         # finalize dwaves
         self.dwaves.finalize()
         
+
+    def _update_com_file(self, var, val):
+        '''update the bed in the com-file with `val`'''
+
+        # com_var can start with "-" to negate value
+        if var.startswith('-'):
+            var = var[1:]
+            val = -val
+        
+        fname = self.com_file
+        with xarray.open_dataset(fname) as ds:
+            data = ds.copy(deep=True)
+            # Problem encountered: variable 'windyu' has a attribute called 'coordinates'
+            # This is problematic because the "coordinates" (eg x and y)  cannot be serialized anymore
+            # ugly solution: remove this attribute only
+            # Fairly better solution: remove all attributes called 'coordinates'
+            # Best solution (but I don't know how): do not create this attribute in the first place
+            for var_name in data.variables.keys():
+                data[var_name].attrs.pop('coordinates',None)    
+            data[var].values = val
+            data.to_netcdf(fname,format="NETCDF3_CLASSIC")
+
+            
